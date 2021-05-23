d7fb2aa1f48b6cfbfb49fb4e739ef3550c25b52c
Jan Robert Darwin <janrdarwin2@gmail.com>
2021-05-23 16:17:43 +0700

    mod ubah

--- pertemuan18/ubah.php
+++ pertemuan18/ubah.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 if (!isset($_SESSION["login"])) {
@@ -26,6 +26,7 @@ if (isset($_POST["submit"])) {
 ?>
 
 <html lang="en">
+
 <head>
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
@@ -36,10 +37,11 @@ if (isset($_POST["submit"])) {
 
 	<title>Ubah Mahasiswa</title>
 </head>
+
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-md-top">
 		<div class="container-fluid">
-		<span class="navbar-brand mb-0 h1">Mahasiswa</span>
+			<span class="navbar-brand mb-0 h1">Mahasiswa</span>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
@@ -49,40 +51,40 @@ if (isset($_POST["submit"])) {
 						<a class="nav-link" aria-current="page" href="index.php">Kembali</a>
 					</li>
 					<li>
-					<a class="nav-link" href="hapus.php?id=<?= $id; ?>" onclick="return confirm('Yakin?')">Hapus</i></a>
+						<a class="nav-link" href="hapus.php?id=<?= $id; ?>" onclick="return confirm('Yakin?')">Hapus</i></a>
 					</li>
 				</ul>
 			</div>
 		</div>
-  </nav>
-	
+	</nav>
+
 	<div class="container-md">
 		<h2>Ubah</h2>
 		<form action="" method="post" enctype="multipart/form-data">
-				<input type="hidden" name="id" id="id" required value="<?= $mhs['id']; ?>">
-				<input type="hidden" name="gambarLama" id="id" value="<?= $mhs['gambar']; ?>">
-				<div class="mb-3">
-					<label class="form-label" for="nrp">NRP:</label>
-					<input class="form-control" type="text" name="nrp" id="nrp" required value="<?= $mhs['nrp']; ?>">
-				</div>
-				<div class="mb-3">
-					<label class="form-label" for="nama">Nama:</label>
-					<input class="form-control" type="text" name="nama" id="nama" required value="<?= $mhs['nama']; ?>">
-				</div>
-				<div class="mb-3">
-					<label class="form-label" for="email">Email:</label>
-					<input class="form-control" type="text" name="email" id="email" required value="<?= $mhs['email']; ?>">
-				</div>
-				<div class="mb-3">
-					<label class="form-label" for="jurusan">Jurusan:</label>
-					<input class="form-control" type="text" name="jurusan" id="jurusan" required value="<?= $mhs['jurusan']; ?>">
-				</div>
-				<div class="mb-3">
-					<label class="form-label" for="gambar">Gambar:</label> <br>
-					<img src="img/<?= $mhs['gambar']; ?>" width="150px" alt="<?= $mhs['gambar']; ?>">
-					<input class="form-control" type="file" name="gambar" id="gambar">
-				</div>
-				<button class="btn btn-primary btn-send" type="submit" name="submit">Ubah</button>
+			<input type="hidden" name="id" id="id" required value="<?= $mhs['id']; ?>">
+			<input type="hidden" name="gambarLama" id="id" value="<?= $mhs['gambar']; ?>">
+			<div class="mb-3">
+				<label class="form-label" for="nrp">NRP:</label>
+				<input class="form-control" type="text" name="nrp" id="nrp" required value="<?= $mhs['nrp']; ?>">
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="nama">Nama:</label>
+				<input class="form-control" type="text" name="nama" id="nama" required value="<?= $mhs['nama']; ?>">
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="email">Email:</label>
+				<input class="form-control" type="text" name="email" id="email" required value="<?= $mhs['email']; ?>">
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="jurusan">Jurusan:</label>
+				<input class="form-control" type="text" name="jurusan" id="jurusan" required value="<?= $mhs['jurusan']; ?>">
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="gambar">Gambar:</label> <br>
+				<img src="img/<?= $mhs['gambar']; ?>" width="150px" alt="<?= $mhs['gambar']; ?>">
+				<input class="form-control" type="file" name="gambar" id="gambar">
+			</div>
+			<button class="btn btn-primary btn-send" type="submit" name="submit">Ubah</button>
 		</form>
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
@@ -93,4 +95,5 @@ if (isset($_POST["submit"])) {
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 	-->
 </body>
+
 </html>
